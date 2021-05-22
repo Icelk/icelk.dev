81466c8872836085c7afc4219a22fb08b44c0bba
Icelk <main@icelk.dev>
2021-05-22 22:10:08 +0200

    Anchor links now scroll smoothly.

--- public/script.js
+++ public/script.js
@@ -77,3 +77,19 @@ const init = async () => {
 }
 
 init()
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault()
+
+        let target = document.getElementById(
+            this.getAttribute("href").substring(1)
+        )
+
+        if (target !== null) {
+            target.scrollIntoView({
+                behavior: "smooth",
+            })
+        }
+    })
+})
