cbdbb8461020bd1a3321dd4219aaa1991c3e03da
Icelk <main@icelk.dev>
2021-05-23 21:11:41 +0200

    Permalink buttons now shows their intent on hover. Also cleaned HTML generated.

--- public/script.js
+++ public/script.js
@@ -108,18 +108,21 @@ const initCopyHeading = () => {
     document.querySelectorAll(queryString).forEach((heading) => {
         let id = heading.getAttribute("id")
         if (id !== undefined) {
-            heading.innerHTML +=
+            let linkButton = document.createElement("span")
+            linkButton.title = "Copy permalink"
+            linkButton.innerHTML =
                 '<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">\
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3" />\
             </svg>'
-            let linkButton = heading.lastElementChild
+            let linkSVG = linkButton.firstElementChild
             linkButton.onclick = () => {
                 let link = `${document.location.href.split("#")[0]}#${id}`
                 navigator.clipboard.writeText(link)
-                linkButton.classList.add("pressed")
-                setTimeout(() => linkButton.classList.remove("pressed"), 150)
+                linkSVG.classList.add("pressed")
+                setTimeout(() => linkSVG.classList.remove("pressed"), 150)
             }
-            linkButton.classList.add("share-button")
+            linkSVG.classList.add("share-button")
+            heading.appendChild(linkButton)
         }
     })
 }
