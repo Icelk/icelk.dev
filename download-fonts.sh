329ef206863b001db954906a6e937fecda5419d8
Icelk <main@icelk.dev>
2021-05-23 14:14:24 +0200

    Added script to download all required fonts. Updated Recursive font version. Added font licenses.

---
+++ download-fonts.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+version="1.078"
+
+curl -L https://github.com/arrowtype/recursive/releases/download/v$version/ArrowType-Recursive-$version.zip -o recursive.zip
+
+mkdir recursive
+unzip recursive.zip -d recursive
+
+font_base="recursive/ArrowType-Recursive-$version"
+font="$font_base/Recursive_Web"
+web="public/fonts"
+
+cp "$font_base/LICENSE.txt" "$web/recursive-license.txt"
+cp -R "$font/woff2_variable_subsets/fonts/" "$web/"
+cp -R "$font/woff2_variable/" "$web/"
+
+rm -rf recursive
+rm recursive.zip
