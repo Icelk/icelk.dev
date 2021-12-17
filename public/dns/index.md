78487b7b232d39892a9210d99271d499df6c4794
Icelk <main@icelk.dev>
2021-12-17 21:07:14 +0100

    Added DNS page with several APIs.

---
+++ public/dns/index.md
@@ -0,0 +1,110 @@
+!> hide
+
+<head>
+    <title>DNS resolver & lookup</title>
+    <meta name="permalinks" content="not-titles"> <!-- part of JS on icelk.dev & kvarn.org, options: disabled|enabled|not-titles -->
+    <meta name="description" content="Fast, log-free and privacy first independent DNS resolver and lookup utility. DNS MX, A, AAAA lookup & DNS over TLS verifier.">
+    <script src="script.js" defer></script>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+When you visit a website (or anything on the internet), you write the name of the site and get an answer.
+Somewhere (probably in a Big Tech data centre), a computer (not unlike the one you're using)
+gets your request and forms a response.
+
+To get the address to send data to, DNS resolvers take the site name and gives you the address. That's what I'm offering. A **privacy first**, fast DNS resolver.
+
+> The service is best in Scandinavia, as that's where the server physically resides.
+
+After seeing the [stats Cloudflare provided](https://blog.cloudflare.com/october-2021-facebook-outage/),
+I realized the DNS resolver I'd been using was keeping logs. This is more private.
+
+> Cloudflare's `1.1.1.1` service is anyway greatly superior in every way (privacy, speed) to Google's offering and especially your ISP's service.
+
+<div id="status" style="text-align: center;">Checking status...</div>
+
+<div id="tools" style="text-align: center;">
+    <div id="toolsHeading">Tools</div>
+    <input id="lookup" class="box" type="text" placeholder="Look up DNS records...">
+    <br>
+    <span id="lookupResult" class="result box" style="display: none;"></span>
+    <span id="getIp" class="box">Reveal my ip</span>
+    <br>
+    <input id="tlsCheck" class="box" type="text" placeholder="Check DoT, <ip>#<name>">
+    <br>
+    <span id="tlsCheckResult" class="result box" style="display: none;"></span>
+</div>
+
+In my perpetual mission of increasing privacy, I've began hosting my own DNS resolver. It does **not** depend on any external resolvers.
+
+> I guarantee to not collect **any** data, no IP logs, no DNS queries, nada.
+
+If your DNS / internet suddenly stops working, please remove the [changes](#setup) and visit this page for updates.
+
+# Service
+
+The IP for this DNS service is **78.69.142.191**.
+
+I offer both DNS on port `53` and [DNS over TLS](https://en.wikipedia.org/wiki/DNS_over_TLS) on port `853`.
+Use DoT for better security and privacy. Using normal DNS (port `53`), middlemen can redirect you to other sites.
+
+# Setup
+
+This is a brief introduction on how to change DNS settings on various devices.
+
+## Mobile devices
+
+In most operating systems, you can change DNS settings on a per network basis.
+Go to the Wi-Fi settings and select the connected network. In that menu, you should find the settings.
+
+## Desktop computers
+
+I recommend installing `Unbound` as a DNS server locally, which caches DNS queries and can more securely fetch them from my server.
+
+### Windows
+
+[Install Unbound](https://nlnetlabs.nl/projects/unbound/download/).
+
+Edit the config file located at `C:\Program Files\Unbound\service.conf`.
+
+At the bottom, add the following:
+
+```
+forward-zone:
+  name: "."
+  forward-tls-upstream: yes
+  forward-addr: 78.69.142.191@853#icelk.dev
+```
+
+Also, find (search in your text editor (e.g. `notepad`)) the line that contains `# tls-win-cert: no`.
+Replace it with `tls-win-cert: yes`. Notice the `#` got removed.
+
+Now, search for and open the program `Services` and restart `Unbound`.
+
+You should now be using my DNS service.
+
+### Unix
+
+Install Unbound using your favourite package manager
+(e.g. `brew` on macOS, `pacman` on Arch derivatives, `apt-get` on Debian derivatives).
+
+Edit the config file located at `/etc/unbound/unbound.conf`.
+At the bottom, add the following:
+
+```
+forward-zone:
+  name: "."
+  forward-tls-upstream: yes
+  forward-addr: 78.69.142.191@853#icelk.dev
+```
+
+Now, you need to add trusted certificates to verify it's me who's responding to your DNS queries.
+On Arch, replace the line containing `tls-cert-bundle` with `tls-cert-bundle: /etc/ssl/certs/ca-certificates.crt`, with the same indentation.
+[Search](https://search.brave.com) for how to do it for your OS.
+
+Check [this](https://wiki.archlinux.org/title/Unbound#Manually_specifying_DNS_servers)
+and [this link](https://wiki.archlinux.org/title/Unbound#Forwarding_using_DNS_over_TLS)
+for more details.
+
+For more details on how to harden this setup and allow captive portals on Linux,
+see my [accompanying article](/articles/dns-unbound-setup.).
---
+++ public/dns/script.js
@@ -0,0 +1,180 @@
+const statusHeading = document.getElementById("status")
+const lookup = document.getElementById("lookup")
+const lookupResult = document.getElementById("lookupResult")
+const getIp = document.getElementById("getIp")
+const tlsCheck = document.getElementById("tlsCheck")
+const tlsCheckResult = document.getElementById("tlsCheckResult")
+
+function setError() {
+    statusHeading.innerText = "Erroneous!"
+    statusHeading.style.color = "red"
+}
+
+function resetLookupResults() {
+    // Clear `lookupResult`
+    lookupResult.innerHTML = ""
+
+    lookupResult.style.display = "none"
+    if (lookupResult.nextElementSibling?.tagName === "BR") {
+        lookupResult.nextElementSibling.remove()
+    }
+}
+function appendLookupResult(response, query) {
+    // Format:
+    // `type value`
+
+    if (lookupResult.childElementCount !== 0) {
+        lookupResult.children[0].before(document.createElement("hr"))
+    }
+
+    const request = document.createElement("p")
+    request.innerText = `${query.substring(0, query.length - 1)}:`
+    request.classList.add("domain")
+    if (lookupResult.children.length === 0) {
+        lookupResult.appendChild(request)
+    } else {
+        lookupResult.children[0].before(request)
+    }
+
+    if (lookupResult.childElementCount === 1) {
+        const clear = document.createElement("button")
+        clear.innerText = "Clear"
+        clear.classList.add("box")
+        clear.addEventListener("click", (_) => {
+            resetLookupResults()
+        })
+        lookupResult.appendChild(clear)
+    }
+
+    response.split("\n").forEach((line) => {
+        if (line.length === 0) {
+            return
+        }
+
+        const type = line.split(" ")[0]
+        const value = line.substring(type.length + 1) ?? ""
+
+        const p = document.createElement("p")
+        const span = document.createElement("span")
+        span.innerText = `${type}: `
+        const result = document.createElement("span")
+        result.addEventListener("click", (_) => {
+            window.getSelection().selectAllChildren(result)
+        })
+        if (type === "MX" || type === "CNAME") {
+            result.addEventListener("dblclick", (_) => {
+                lookup.value = result.innerText
+                lookupHandler()
+            })
+        }
+        result.innerText = value
+
+        p.appendChild(span)
+        p.appendChild(result)
+
+        request.after(p)
+    })
+    if (response.trim().length === 0) {
+        const notFound = document.createElement("p")
+        notFound.innerText = "No results found."
+        request.after(notFound)
+    }
+
+    lookupResult.style.display = ""
+    if (lookupResult.nextElementSibling?.tagName !== "BR") {
+        lookupResult.after(document.createElement("br"))
+    }
+}
+
+fetch("lookup?domain=icelk.dev.").then((response) => {
+    if (response.ok) {
+        statusHeading.innerText = "Service operational"
+    } else {
+        setError()
+    }
+})
+
+function lookupHandler() {
+    const value = lookup.value ?? ""
+    const fqdn = value.endsWith(".") ? value : `${value}.`
+
+    const domain = encodeURIComponent(fqdn.trim())
+
+    if (domain === ".") {
+        resetLookupResults()
+        return
+    }
+
+    fetch(`lookup?domain=${domain}`).then((response) => {
+        if (!response.ok) {
+            lookupResult.style.display = ""
+            appendLookupResult("", domain)
+            return
+        }
+
+        response.text().then((response) => {
+            appendLookupResult(response, domain)
+        })
+    })
+}
+
+addListeners(lookup, lookupHandler)
+addListeners(tlsCheck, tlsCheckHandler)
+
+const ipResolver = (_) => {
+    fetch("/ip").then(async (response) => {
+        if (response.ok) {
+            const text = await response.text()
+            getIp.innerText = text
+            getIp.classList.add("revealed")
+        } else {
+            getIp.innerText = "Failed to get your IP"
+            getIp.classList.add("revealed")
+        }
+
+        getIp.removeEventListener("click", ipResolver)
+    })
+}
+getIp.addEventListener("click", ipResolver)
+
+function tlsCheckHandler() {
+    const ip = tlsCheck.value.split("#")[0]
+    const name = tlsCheck.value.split("#")[1] ?? ""
+
+    tlsCheckResult.innerHTML = ""
+    tlsCheckResult.style.display = "none"
+
+    if (ip === "" || name === "") {
+        if (ip !== "" && name === "") {
+            tlsCheckResult.innerHTML =
+                "<p>Please use the format <code>ip#name</code>, e.g. <code>78.69.142.191#icelk.dev</code>.</p>"
+            tlsCheckResult.style.display = ""
+        }
+        return
+    }
+
+    fetch(`check-dns-over-tls?ip=${ip}&name=${name}`).then(async (response) => {
+        if (!response.ok) {
+            tlsCheckResult.innerHTML = `<p>Server response: ${response.status} ${response.statusText}</p>`
+        } else {
+            const text = await response.text()
+            if (text === "supported") {
+                tlsCheckResult.innerHTML = "<p>Supported</p>"
+            } else if (text === "unsupported") {
+                tlsCheckResult.innerHTML = "<p>Unsupported / wrong host name</p>"
+            } else {
+                tlsCheckResult.innerText = `<p>${text}</p>`
+            }
+        }
+        tlsCheckResult.style.display = ""
+    })
+}
+
+function addListeners(element, handler) {
+    // element.addEventListener("change", (_) => handler())
+    element.addEventListener("keydown", (ev) => {
+        if (ev.code === "Enter") {
+            handler()
+        }
+    })
+}
