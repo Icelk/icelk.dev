d05c445c19cd9f809663ff8cff39d086c51132bb
Icelk <main@icelk.dev>
2022-09-14 18:30:44 +0200

    Quizlet: Added support for multiple options for answers and added a list for failed words.

--- public/quizlet-learn/script.js
+++ public/quizlet-learn/script.js
@@ -10,9 +10,12 @@ let page_error = document.getElementById("page-error")
 let words = []
 let active_words = []
 let failed_words = []
-let answer = null
+let answers = null
+let source_words = null
 // make the user type the answer to practise
 let checking = false
+let round = 1
+let learn_children = learn.children.length
 
 page_input.addEventListener("keydown", async (e) => {
     if (e.key === "Enter") {
@@ -34,10 +37,11 @@ word_input.addEventListener("keydown", (e) => {
         word_input.value = ""
         if (active_words.length === 0) {
             if (failed_words.length === 0) {
-                word_result.innerText += ` Done. You completed every word! Redoing all words.`
+                word_result.innerText += ` You completed every word! Redoing all words.`
                 active_words = [...words]
             } else {
-                word_result.innerText += ` Done. Failed ${failed_words.length}/${words.length}. Redoing the failed words.`
+                word_result.innerText += ` Failed ${failed_words.length}/${words.length}. Redoing the failed words.`
+                append_failed()
                 active_words = [...failed_words]
                 failed_words = []
             }
@@ -46,23 +50,31 @@ word_input.addEventListener("keydown", (e) => {
     }
 
     if (checking) {
-        // make sure the value has updated
-        setTimeout(() => {
-            if (word_input.value === answer) {
-                word_result.innerText = "Correct!"
-                checking = false
-                success()
-            }
-        }, 0)
+        if (e.key === "Enter") {
+            word_result.innerText = "Keep going!"
+            checking = false
+            success()
+        } else {
+            // make sure the value has updated
+            setTimeout(() => {
+                if (answers.some((answer) => word_input.value === answer)) {
+                    word_result.innerText = "Correct!"
+                    checking = false
+                    success()
+                }
+            }, 0)
+        }
         return
     }
     word_result.innerText = "Keep going!"
     if (e.key === "Enter") {
-        if (word_input.value === answer) {
+        if (answers.some((answer) => word_input.value === answer)) {
             word_result.innerText = "Correct!"
         } else {
-            failed_words.push([word_display.innerText, answer])
-            word_result.innerText = `Wrong. Right answer was ${answer}. Please type it to continue.`
+            failed_words.push([source_words, answers])
+            word_result.innerText = `Right answer was ${answers
+                .map((s) => `"${s}"`)
+                .join(" or ")}. Type it to continue.`
             checking = true
             return
         }
@@ -80,7 +92,12 @@ async function get_page(page) {
     let body = await response.text()
     let w = []
     body.split("\n").forEach((line) => {
-        w.push(line)
+        w.push(
+            line
+                .split("|")
+                .map((s) => s.trim())
+                .filter((s) => s !== "")
+        )
         if (w.length >= 2) {
             words.push(w)
             w = []
@@ -90,19 +107,25 @@ async function get_page(page) {
 }
 function start_words() {
     if (words.length === 0) {
+        page_error.innerText = "The URL doesn't point to a Quizlet word list."
         return
     }
     active_words = [...words]
+    round = 1
+    while (learn.children.length > learn_children) {
+        learn.lastElementChild.remove()
+    }
     learn.style.display = ""
     word_result.innerText = "Feel free to start typing. Press enter when completed."
     random_word()
 }
 function random_word() {
     let i = Math.floor(Math.random() * active_words.length)
-    let word = active_words[i]
+    let pair = active_words[i]
     active_words.splice(i, 1)
-    answer = word[1]
-    word_display.innerText = word[0]
+    source_words = pair[0]
+    answers = pair[1]
+    word_display.innerText = pair[0].join(", ")
 }
 function swap_words() {
     let w = [...words]
@@ -110,4 +133,25 @@ function swap_words() {
         const word = w[i]
         words[i] = [word[1], word[0]]
     }
+}
+
+function append_failed() {
+    let div = document.createElement("div")
+    div.classList.add("failed-words")
+    let h = document.createElement("h1")
+    h.innerText = `Round ${round} failed words`
+    round += 1
+    div.appendChild(h)
+    let grid = document.createElement("div")
+    grid.classList.add("failed-words-grid")
+    failed_words.forEach(([words, answers]) => {
+        let word = document.createElement("span")
+        word.innerText = words.join(" or ")
+        let answer = document.createElement("span")
+        answer.innerText = answers.join(" or ")
+        grid.appendChild(word)
+        grid.appendChild(answer)
+    })
+    div.appendChild(grid)
+    learn.appendChild(div)
 }
