ce6811e141cd51ea63882036efe58e223a7fa8c4
Icelk <main@icelk.dev>
2023-09-14 21:22:12 +0200

    Update to new kvarn proper-quic branch!

--- publish.sh
+++ publish.sh
@@ -3,4 +3,5 @@
 rsync -rvPhL --del --exclude '**target**' --exclude '**.pem' --exclude '**private' --exclude '**rsync-ignore**' --exclude 'lets-encrypt-credentials**' --exclude "doc/public/" --include "doc/public/index.html" --include "doc/public/style.css" --exclude "**.git" ./ icelk.dev:~/kvarn/icelk.dev/
 cd server
 cargo build --release
+ssh icelk.dev "mkdir -p kvarn/icelk.dev/server/target/release"
 rsync -rvPhL ./target/release/kvarn-icelk icelk.dev:~/kvarn/icelk.dev/server/target/release/kvarn-icelk
